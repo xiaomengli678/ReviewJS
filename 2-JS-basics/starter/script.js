8b82414f09de0476eebc3eebe257a893d56612ab
Xiaomeng Li <xiaomengli678@gmail.com>
2020-05-30 22:13:05 -0600

    add the git ignore

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,67 @@
+// // var firstName = "John";
+// //console.log(firstName);
+
+// var lastName = "Smith";
+// var age = 28;
+
+// var fullAge = true;
+// //console.log(fullAge);
+
+// var job;
+// //console.log(job)
+
+// job = "Teacher";
+// //console.log(job);
+
+// // learn javascript
+// /* 
+// * Variable mutation and type coercion 
+// */
+
+// var firstName = "John";
+// var age = 28;
+
+// var job, isMarried;
+// job = "teacher";
+// isMarried = false;
+
+// console.log(firstName + " is a " + age)
+
+// // Variable mutation
+// age = "twenty eight"
+// job = "driver";
+
+// //alert(firstName + " is a " + job)
+// var lastName = prompt("what is his last name?");
+// console.log(firstName + " " + lastName);
+
+// // Math Operators
+// var h = 2018 - 27;
+// console.log(h + 2);
+// console.log(h * 2);
+// console.log(h / 10);
+
+// // Logical operators
+// var ageA = 1;
+// var ageB = 5;
+// var ageOlder = ageA > ageB;
+// console.log(ageOlder);
+
+// // typeof operator
+// console.log(typeof ageOlder);
+// console.log(typeof "lalala");
+// console.log(typeof ageB);
+// console.log(typeof l);
+
+// // Multiple assignments
+// var x, y;
+// x = y = (26 + 34) / 7;
+// console.log(x, y);
+
+// // More operators
+// x = x * 2;
+// x *= 2;
+// x += 1;
+// x++;
+
+
