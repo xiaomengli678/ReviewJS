3440f2bb9ff614c2bfb518703fa7e56082b1135c
Xiaomeng Li <xiaomengli678@gmail.com>
2020-06-04 20:36:11 -0600

    object learned

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -64,7 +64,7 @@
 // x += 1;
 // x++;
 
-var firstName = 'John';
+// var firstName = 'John';
 // var civilStatus = 'single';
 
 // if (civilStatus === 'married'){
@@ -96,7 +96,7 @@ var firstName = 'John';
 //     console.log('John\'s BMI is higher than Mark\'s.')
 // }
 
-var age = 16;
+// var age = 16;
 // if (age < 13){
 //     console.log(firstName + ' is a boy. ');
 // } else if (age >= 13 && age < 20){
@@ -151,23 +151,116 @@ var age = 16;
 //     console.log('The == operator does type coercion !')
 // }
 
-var s1 = (129 + 120 + 103) / 3;
-var s2 = (116 + 94 + 123) / 3;
-var s3 = (97 + 134 + 105) / 3;
-console.log(s1, s2, s3);
-if (s1 > s2){
-    console.log("s1\'s " + s1 + " point is higher");
-} else if (s2 > s1) {
-    console.log("s2\'s " + s2 + " point is higher");
-} else {
-    console.log("draw");
+// var s1 = (129 + 120 + 103) / 3;
+// var s2 = (116 + 94 + 123) / 3;
+// var s3 = (97 + 134 + 105) / 3;
+// console.log(s1, s2, s3);
+// if (s1 > s2){
+//     console.log("s1\'s " + s1 + " point is higher");
+// } else if (s2 > s1) {
+//     console.log("s2\'s " + s2 + " point is higher");
+// } else {
+//     console.log("draw");
+// }
+
+// if (s1 > s2 && s1 > s3){
+//     console.log("s1 rules");
+// } else {
+//     console.log("s1 doesn\'t rule")
+// }
+
+function calculateAge(birthYear) {
+    return 2020 - birthYear;
+}
+var ageMe = calculateAge(1992);
+//console.log(ageMe);
+
+function yearsUntil(birthYear, name){
+    var age = calculateAge(birthYear);
+    var retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(name + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(name + ' is already retired.');
+    }
+}
+//yearsUntil(1390, 'John')
+
+//function expression
+var whatDo = function(job, name){
+    switch(job){
+        case 'teacher':
+            return name + ' teaches coding';
+        case 'driver':
+            return name + ' drives a cab in NM';
+        default:
+            return name + ' does else';
+    }
 }
 
-if (s1 > s2 && s1 > s3){
-    console.log("s1 rules");
-} else {
-    console.log("s1 doesn\'t rule")
-}
+//console.log(whatDo('teacher', 'Jack'));
+
+// Mutate array data
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1992, 1991, 1930);
+// console.log(names[2]);
+// console.log(names.length);
+names[1] = 'Jack';
+names[names.length] = 'Mary';
+//console.log(names)
+// var john = ['John', 1992, 'teacher', false];
+
+// john.push('blue');
+// john.unshift('Mr.') //appendleft
+// john.pop();
+// john.shift(); // popleft
+
+// console.log(john);
+
+// console.log(john.indexOf(1990));
+// var isD = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John is a designer';
+// console.log(isD);
+
+// Objects and properties
+
+// object literal
+// var john = {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     birthYear: 1990,
+//     family: ['mother', 'father'],
+//     job: 'teacher',
+//     isMarried: false
+// };
+// console.log(john.family);
+// console.log(john['family']);
+// john.job = 'designer';
+// john['isMarried'] = true;
+//console.log(john);
+
+// new object syntax
+var jane = new Object();
+jane.name = 'Jane';
+jane['family'] = ['father', 'mother'];
+//console.log(jane)
+
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['mother', 'father'],
+    job: 'teacher',
+    isMarried: false,
+    ageCal: function(){
+        this.age = 2020 - this.birthYear;
+    }
+};
+
+john.ageCal();
+// console.log(john);
+
+//john.age = john.ageCal();
+
 
 
 
